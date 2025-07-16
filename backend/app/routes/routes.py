ec2373a61f2eadf29a22c28703b3b4abdc666132
Bouali <youssefbouali206@gmail.com>
2025-07-16 10:14:29 +0100

    Update User Model

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 from app.controllers.user import register, login
 
-auth_bp = Blueprint('all_bp', __name__)
-auth_bp.route('/register', methods=['POST'])(register)
-auth_bp.route('/login', methods=['POST'])(login)
+all_bp = Blueprint('all_bp', __name__)
+all_bp.route('/register', methods=['POST'])(register)
+all_bp.route('/login', methods=['POST'])(login)
 
 __all__ = ['all_bp']
