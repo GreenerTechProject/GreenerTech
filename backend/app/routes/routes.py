47a922fb479cdd675bfd3adb327a26653fee4d07
Bouali <youssefbouali206@gmail.com>
2025-07-16 10:41:58 +0100

    Update User Model

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -6,7 +6,8 @@ all_bp.route('/register', methods=['POST'])(register)
 all_bp.route('/login', methods=['POST'])(login)
 
 all_bp.route('/user', methods=['PUT'])(update_user)
-auth_bp.route('/user/<int:user_id>', methods=['PUT'])(update_user)
+all_bp.route('/user/<int:user_id>', methods=['PUT'])(update_user)
+all_bp.route('/user', methods=['GET'])(get_user)
 all_bp.route('/user', methods=['DELETE'])(delete_user)
 
 __all__ = ['all_bp']
