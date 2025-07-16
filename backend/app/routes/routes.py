f0cf11d0991f214f9097e55ff7e48a47a1c859e1
Bouali <youssefbouali206@gmail.com>
2025-07-16 10:09:13 +0100

    Update User Model

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -5,8 +5,4 @@ auth_bp = Blueprint('auth', __name__)
 auth_bp.route('/register', methods=['POST'])(register)
 auth_bp.route('/login', methods=['POST'])(login)
 
-auth_bp.route('/user', methods=['PUT'])(update_user)
-auth_bp.route('/user', methods=['GET'])(read_user)
-auth_bp.route('/user', methods=['DELETE'])(delete_user)
-
 __all__ = ['all_bp']
