303c471134757e79e4f8b673662691633eb5248e
Bouali <youssefbouali206@gmail.com>
2025-07-16 09:34:17 +0100

    Update User Model

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -9,5 +9,5 @@ def login():
     data = request.get_json()
     user = User.query.filter_by(email=data['email']).first()
     if user and user.password == data['password']:
-        return jsonify({"message": "Login successful222222222"}), 200
+        return jsonify({"message": "Login successful"}), 200
     return jsonify({"message": "Invalid credentials"}), 401
