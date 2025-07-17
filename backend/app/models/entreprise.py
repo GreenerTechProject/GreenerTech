b58127e78a1d7e53fc3e42ae409de9e35d09f930
brahim-bannany <brahimbannany2021@gmail.com>
2025-07-17 17:00:17 +0100

    add Entreprise model and controlerw

---
+++ backend/app/models/entreprise.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String ,ForeignKey
+from database.config import db
+
+class Entreprise(db.Model):
+    __tablename__ = "entreprises"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nom = Column(String, nullable=False)
+    id_user = Column(Integer, ForeignKey('users.id'), nullable=False)
+    status_juridique = Column(String)
+    adresse = Column(String)
+    id_fiscale = Column(Integer)
+    email = Column(String)
--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from app.controllers.user import register, login, delete_user, update_user, get_user
+from app.controllers.entreprise import create_entreprise, get_my_entreprise, update_entreprise, delete_entreprise
 
 all_bp = Blueprint('all_bp', __name__)
 all_bp.route('/register', methods=['POST'])(register)
@@ -7,5 +8,13 @@ all_bp.route('/login', methods=['POST'])(login)
 all_bp.route('/user', methods=['DELETE'])(delete_user)
 all_bp.route('/user', methods=['PUT'])(update_user)
 all_bp.route('/user', methods=['GET'])(get_user)
+
+
+
+all_bp.route('/entreprise', methods=['POST'])(create_entreprise)
+all_bp.route('/entreprise', methods=['GET'])(get_my_entreprise)
+all_bp.route('/entreprise', methods=['PUT'])(update_entreprise)
+all_bp.route('/entreprise', methods=['DELETE'])(delete_entreprise)
+
 
 __all__ = ['all_bp']
