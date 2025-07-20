8437f25a443cbb407201617ffed4bc0cd30da7b7
Bouali <youssefbouali206@gmail.com>
2025-07-20 18:38:50 +0100

    Create Bilans

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -3,6 +3,8 @@ from app.controllers.user import register, login, get_user, update_user, delete_
 from app.controllers.entreprise import create_entreprise, get_entreprise, update_entreprise, delete_entreprise
 from app.controllers.domaine import create_domaine, get_domaine, get_all_domaines, update_domaine, delete_domaine
 
+from app.controllers.bialn import create_bialn, get_bialn, get_all_bialns, update_bialn, delete_bialn
+
 all_bp = Blueprint('all_bp', __name__)
 all_bp.route('/register', methods=['POST'])(register)
 all_bp.route('/login', methods=['POST'])(login)
@@ -22,6 +24,18 @@ all_bp.route('/domaine/<int:id>', methods=['GET'])(get_domaine)
 all_bp.route('/domaine', methods=['GET'])(get_all_domaines)
 all_bp.route('/domaine/<int:id>', methods=['PUT'])(update_domaine)
 all_bp.route('/domaine/<int:id>', methods=['DELETE'])(delete_domaine)
+
+
+
+
+
+
+
+all_bp.route('/bialn', methods=['POST'])(create_bialn)
+all_bp.route('/bialn/<int:id>', methods=['GET'])(get_bialn)
+all_bp.route('/bialn', methods=['GET'])(get_all_bialns)
+all_bp.route('/bialn/<int:id>', methods=['PUT'])(update_bialn)
+all_bp.route('/bialn/<int:id>', methods=['DELETE'])(delete_bialn)
 
 
 __all__ = ['all_bp']
