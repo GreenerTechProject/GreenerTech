039ba4b5ac8a6d07467382ea10169b2c68a31d45
Bouali <youssefbouali206@gmail.com>
2025-07-24 16:13:21 +0100

    Add video Streaming

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,6 @@
-import threading
-import subprocess
-from app import create_app  # Import your Flask app
+from app import create_app
 
 app = create_app()
 
-def run_flask():
-    app.run(host="0.0.0.0", port=5000)
-
-def run_video_stream_service():
-    import app.services.video_streaming_service
-
 if __name__ == "__main__":
-    t1 = threading.Thread(target=run_flask)
-    t2 = threading.Thread(target=run_video_stream_service)
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
+    app.run(debug=True, host="0.0.0.0", port=5000)
