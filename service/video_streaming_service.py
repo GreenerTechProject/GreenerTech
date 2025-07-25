8e4b41d9776b9deaf32d9618021d355f899e258a
Bouali <youssefbouali206@gmail.com>
2025-07-25 12:10:36 +0100

    Add video Streaming

--- service/video_streaming_service.py
+++ service/video_streaming_service.py
@@ -38,8 +38,8 @@ class RelayStreamTrack(VideoStreamTrack):
             raise Exception("No video stream and no fallback image found!")
         if self.fallback_frame is None:
             print(f"❌ Failed to load fallback image from: {fallback_path}")
-        else:
-            print(f"✅ Fallback image loaded. Shape: {self.fallback_frame.shape}")
+        #else:
+        #    print(f"✅ Fallback image loaded. Shape: {self.fallback_frame.shape}")
 
 
         #while latest_frame is None:
