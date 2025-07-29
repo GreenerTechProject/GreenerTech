2e01735c338ccf825d50bd7f187e5433905dfd3c
LinaAitIder <linaitider@gmail.com>
2025-07-29 21:52:06 +0100

    Changes on the backend

--- front-end-ui/src/components/navigation/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MainNavigation } from './MainNavigation';
-export { default as FloatingNavigation } from './FloatingNavigation';
-export { useFloatingNavigation } from './useFloatingNavigation';
