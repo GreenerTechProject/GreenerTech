1a00743d1852591510568b3b710a744419568b1c
Bouali <youssefbouali206@gmail.com>
2025-07-31 00:42:37 +0100

    Fix entreprise creation front api endpoint

--- front-end-ui/src/components/directeur-setUp/EntrepriseStep.jsx
+++ front-end-ui/src/components/directeur-setUp/EntrepriseStep.jsx
@@ -27,7 +27,7 @@ export default function EntrepriseStep({ setStep, setEntrepriseId, onSkip }) {
     }
 
     try {
-      const response = await fetch("/api/entreprise/create", {
+      const response = await fetch("/api/entreprise", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
