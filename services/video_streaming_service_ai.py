26c14041688f42be722d335511101454e90bf54e
Bouali <youssefbouali206@gmail.com>
2025-08-03 18:53:36 +0100

    Add Services container

--- services/video_streaming_service_ai.py
+++ services/video_streaming_service_ai.py
@@ -2,7 +2,7 @@ import asyncio
 import cv2
 import numpy as np
 from aiohttp import web, WSMsgType
-from aiortc import RTCPeerConnection, VideoStreamTrack, RTCSessionDescription
+from aiortc import RTCPeerConnection, VideoStreamTrack, RTCSessionDescription, RTCConfiguration, RTCIceServer
 from av import VideoFrame
 from cv2 import QRCodeDetector
 #from detectobjects import detect_frame
@@ -136,11 +136,15 @@ async def offer(request):
     params = await request.json()
     offer = params["offer"]
 
-    pc = RTCPeerConnection(configuration={
-        "iceServers": [
-            { "urls": "stun:stun.l.google.com:19302" }
-        ]
-    })
+
+    pc = RTCPeerConnection(
+        configuration=RTCConfiguration(
+            iceServers=[
+                RTCIceServer(urls=["stun:stun.l.google.com:19302"])
+            ]
+        )
+    )
+
 
     @pc.on("connectionstatechange")
     async def on_connectionstatechange():
