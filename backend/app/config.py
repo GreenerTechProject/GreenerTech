513f63562e30b372149d9840339c9a0930ba7978
Bouali <youssefbouali206@gmail.com>
2025-08-05 23:30:18 +0100

    Fix director_valide

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,10 +4,10 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')  # à personnaliser
     MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
     MAIL_PORT = 2525
-    # MAIL_USERNAME =os.getenv('MAIL_USERNAME')
-    # MAIL_PASSWORD =os.getenv('MAIL_PASSWORD')
-    MAIL_USERNAME ="e061372fd9c1c2"
-    MAIL_PASSWORD ="4ede236e82a032"
+    MAIL_USERNAME =os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD =os.getenv('MAIL_PASSWORD')
+    #MAIL_USERNAME ="e061372fd9c1c2"
+    #MAIL_PASSWORD ="4ede236e82a032"
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_DEFAULT_SENDER = 'noreply@greenertech.com'
