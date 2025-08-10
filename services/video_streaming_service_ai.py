f0c68d730f865ebc42c80e5432660a12af9312e6
Bouali <youssefbouali206@gmail.com>
2025-08-10 23:00:04 +0100

    Fix video streaming service

--- services/video_streaming_service_ai.py
+++ services/video_streaming_service_ai.py
@@ -40,7 +40,7 @@ class RelayStreamTrack(VideoStreamTrack):
         #frame_to_use = detect_frame(latest_frame) if latest_frame is not None else self.fallback_frame
         
         #_, Billan_dicts = predict_frame(latest_frame) if latest_frame is not None else self.fallback_frame, 111
-        print (Billan_dicts)
+        #print (Billan_dicts)
         # frame_to_use = detected_frame
         if frame_to_use is None:
             raise Exception("No video stream and no fallback image found!")
