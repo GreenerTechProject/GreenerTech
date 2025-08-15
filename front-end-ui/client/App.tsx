e51c7211731ff81fc9bfb1695c6c01dc624b13d9
Bouali <youssefbouali206@gmail.com>
2025-08-15 23:47:19 +0100

    Fix multi import robot control

--- front-end-ui/client/App.tsx
+++ front-end-ui/client/App.tsx
@@ -30,7 +30,6 @@ import Interventions from "./pages/Interventions";
 import Surveillance from "./pages/Surveillance";
 import Profile from "./pages/Profile";
 import ProfileEdit from "./pages/ProfileEdit";
-import RobotControl from "./pages/RobotControl";
 import NotFound from "./pages/NotFound";
 import TechnicienSupProfile from "./pages/TechnicienSupProfile";
 
