4ec9e7e29ef9e017a6dc1e4c2648ae05f00ee637
Bouali <youssefbouali206@gmail.com>
2025-08-15 23:55:20 +0100

    Fix rapports

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -25,7 +25,8 @@ from app.controllers.alerte import (
     get_alertes, get_alerte, update_alerte,
     get_alertes_by_entreprise, get_alertes_by_director_entreprise
 )
-from app.controllers.rapport import create_rapport, get_rapport, update_rapport, delete_rapport, get_rapports_by_director_entreprise
+#from app.controllers.rapport import create_rapport, get_rapport, update_rapport, delete_rapport, get_rapports_by_director_entreprise
+from app.controllers.rapport import create_rapport, get_rapport, get_rapports_by_director_entreprise
 
 
 all_bp = Blueprint('all_bp', __name__)
@@ -175,8 +176,8 @@ all_bp.route('/notifications/vue/<int:id>', methods=['PUT'])(mark_notification_a
 all_bp.route('/rapport', methods=['POST'])(create_rapport)
 # all_bp.route('/rapport', methods=['GET'])(get_all_rapports)
 all_bp.route('/rapport', methods=['GET'])(get_rapports_by_user)
-all_bp.route('/rapport/<int:id>', methods=['PUT'])(update_rapport)
-all_bp.route('/rapport/<int:id>', methods=['DELETE'])(delete_rapport)
+#all_bp.route('/rapport/<int:id>', methods=['PUT'])(update_rapport)
+#all_bp.route('/rapport/<int:id>', methods=['DELETE'])(delete_rapport)
 all_bp.route('/rapport/director-enterprise', methods=['GET'])(get_rapports_by_director_entreprise)
 
 
