021f9c657f70d7fd04addc132d9d05362051c7f1
Bouali <youssefbouali206@gmail.com>
2025-08-19 23:28:59 +0100

    Up ia model

--- services/multirobotcam/video_streaming_service_ai_webrtc.py
+++ services/multirobotcam/video_streaming_service_ai_webrtc.py
@@ -19,7 +19,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-from ia.models.ALL import detect_frame, predict_frame
+from ALL import detect_frame, predict_frame
 
 
 import asyncpg
