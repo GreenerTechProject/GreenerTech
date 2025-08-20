295781025aa6379e98b40b03cc6fb876c23ab4d6
Bouali <youssefbouali206@gmail.com>
2025-08-20 03:15:51 +0100

    Up videostream ia

--- ia/models/ALL.py
+++ ia/models/ALL.py
@@ -12,7 +12,7 @@ def detect_frame(bgr_frame):
         classes = result.boxes.cls.cpu().numpy()
         confidences = result.boxes.conf.cpu().numpy()
         for box,cls,conf in zip(boxes,classes,confidences):
-            if conf>0.8:
+            if conf>0.5:
                 x1,y1,x2,y2=map(int,box)
                 cv2.rectangle(bgr_frame,(x1,y1),(x2,y2),(255,0,0),2)
                 cv2.rectangle(bgr_frame,(x1,y1-25),(x1+80,y1),(255,0,0),-1)
