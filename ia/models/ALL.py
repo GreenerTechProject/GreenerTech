0155d14c6349a7b8610316a5e60f08df9785d6f9
Bouali <youssefbouali206@gmail.com>
2025-08-27 09:36:41 +0100

    Update Streaming with ai

--- ia/models/ALL.py
+++ ia/models/ALL.py
@@ -24,7 +24,7 @@ def detect_frame(bgr_frame):
         classes = result.boxes.cls.cpu().numpy()
         confidences = result.boxes.conf.cpu().numpy()
         for box,cls,conf in zip(boxes,classes,confidences):
-            if conf>0.5:
+            if conf>0.7:
                 x1,y1,x2,y2=map(int,box)
                 cv2.rectangle(bgr_frame,(x1,y1),(x2,y2),(255,0,0),2)
                 cv2.rectangle(bgr_frame,(x1,y1-25),(x1+80,y1),(255,0,0),-1)
@@ -53,7 +53,7 @@ def predict_frame(bgr_frame):
         classes = result.boxes.cls.cpu().numpy()
         confidences = result.boxes.conf.cpu().numpy()
         for box,cls,conf in zip(boxes,classes,confidences):
-            if conf>0.5:
+            if conf>0.7:
                 x1,y1,x2,y2=map(int,box)
                 info_img = frame_yolo[y1:y2,x1:x2]
                 if info_img.size>0:
