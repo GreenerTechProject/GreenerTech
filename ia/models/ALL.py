f92856386b27f957acc3fd59cfbdb8435f7df669
Bouali <youssefbouali206@gmail.com>
2025-08-27 07:35:37 +0100

    Update Streaming with ai

--- ia/models/ALL.py
+++ ia/models/ALL.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 import torch
 from MaClass import TomatoClassifier,process_image
 device = '0' if torch.cuda.is_available() else 'cpu'
+#model = YOLO("my_model.pt").to(device)  #load the Model
 model = YOLO("new_yolo_model.pt").to(device)  #load the Model
 """
 def detect_frame(bgr_frame):
@@ -23,7 +24,7 @@ def detect_frame(bgr_frame):
         classes = result.boxes.cls.cpu().numpy()
         confidences = result.boxes.conf.cpu().numpy()
         for box,cls,conf in zip(boxes,classes,confidences):
-            if conf>0.7:
+            if conf>0.6:
                 x1,y1,x2,y2=map(int,box)
                 cv2.rectangle(bgr_frame,(x1,y1),(x2,y2),(255,0,0),2)
                 cv2.rectangle(bgr_frame,(x1,y1-25),(x1+80,y1),(255,0,0),-1)
@@ -52,7 +53,7 @@ def predict_frame(bgr_frame):
         classes = result.boxes.cls.cpu().numpy()
         confidences = result.boxes.conf.cpu().numpy()
         for box,cls,conf in zip(boxes,classes,confidences):
-            if conf>0.7:
+            if conf>0.6:
                 x1,y1,x2,y2=map(int,box)
                 info_img = frame_yolo[y1:y2,x1:x2]
                 if info_img.size>0:
