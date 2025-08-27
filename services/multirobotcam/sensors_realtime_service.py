f697c71a8d98413ef4be731c00810d8d699a420a
Bouali <youssefbouali206@gmail.com>
2025-08-27 10:43:41 +0100

    Update Streaming with ai

--- services/multirobotcam/sensors_realtime_service.py
+++ services/multirobotcam/sensors_realtime_service.py
@@ -1,6 +1,6 @@
 from aiohttp import web, WSMsgType
 import json
-from multirobotcam.video_streaming_service_webrtc_ai import get_stream_data  # use the updated dict
+from multirobotcam.video_streaming_service_ai_webrtc import get_stream_data  # use the updated dict
 from PIL import Image
 from datetime import datetime
 import requests
