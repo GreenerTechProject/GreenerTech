8d4ab8abfd9f6ff094fb7dd2eceea059c2996495
Bouali <youssefbouali206@gmail.com>
2025-08-28 12:45:55 +0100

    Fix Update robot

--- backend/app/controllers/robot.py
+++ backend/app/controllers/robot.py
@@ -74,7 +74,8 @@ def update_robot(current_user):
     try:
         robot.nom = data.get('nom', robot.nom)
         robot.referance = data.get('referance', robot.referance)
-        robot.id_entreprise = data.get('id_entreprise', robot.id_entreprise)
+        #robot.id_entreprise = data.get('id_entreprise', robot.id_entreprise)
+        robot.id_entreprise = current_user.id_entreprise
         db.session.commit()
         return jsonify(robot.to_dict()), 200
     except Exception as e:
